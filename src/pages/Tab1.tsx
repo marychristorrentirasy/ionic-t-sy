86dae388a272ed5624728815612dc2b3159d455f
marychristorrentirasy <marychristorrentirasy@gmail.com>
2024-03-12 08:36:23 +0800

    added quick facts botton

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, 
-  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
+  IonAlert, IonButton  } from '@ionic/react';
 
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
@@ -25,9 +25,21 @@ const Tab1: React.FC = () => {
         <IonCardTitle>marychris</IonCardTitle>
         <IonCardSubtitle>student</IonCardSubtitle>
       </IonCardHeader>
-
       <IonCardContent> Nothing more, nothing less.</IonCardContent>
     </IonCard>
+
+ 
+    <IonButton id="present-alert" expand="full">Quick facts</IonButton>
+    
+      <IonAlert
+        trigger="present-alert"
+        header="A Short Title Is Best"
+        subHeader="A Sub Header Is Optional"
+        message="A message should be a short, complete sentence."
+        buttons={['Action']}
+      ></IonAlert>
+
+
       </IonContent>
     </IonPage>
   );
